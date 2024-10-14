70b5ee35f259eb2489f1d9cdebb0bd042b0bc548
datnguyen12032003 <danptce171966@fpt.edu.vn>
2024-10-14 23:44:50 +0700

    edit cartadapter

--- FoodOrderApp/app/src/main/java/com/example/food_order_app/Adapters/CartAdapter.java
+++ FoodOrderApp/app/src/main/java/com/example/food_order_app/Adapters/CartAdapter.java
@@ -3,14 +3,19 @@ package com.example.food_order_app.Adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.food_order_app.Fragments.CartFragment;
 import com.example.food_order_app.Models.Cart;
 import com.example.food_order_app.Models.FoodItem;
 import com.example.food_order_app.R;
@@ -20,6 +25,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder> {
@@ -32,6 +38,8 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
         void onEncreaseClick(Cart cart);
 
         void onDecreseClick(Cart cart);
+
+        void onDeleteClick(Cart cart);
     }
 
     public CartAdapter(List<Cart> cartItems, OnItemClickListener onItemClickListener) {
@@ -74,6 +82,7 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
                     holder.foodQuantity.setText(String.valueOf(cartItem.getQuantity()));
                 }
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 throw error.toException();
@@ -95,7 +104,12 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
             }
         });
 
-
+        holder.btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onItemClickListener.onDeleteClick(cartItem);
+            }
+        });
     }
 
     @Override
@@ -110,7 +124,7 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
     public class CartViewHolder extends RecyclerView.ViewHolder {
         private ImageView foodImage;
         private TextView foodName, foodCategory, foodPrice, foodRating, foodQuantity;
-        private ImageButton btnEncrease, btnDecrese;
+        private ImageButton btnEncrease, btnDecrese, btnDelete;
 
         public CartViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -122,6 +136,7 @@ public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder
             foodQuantity = itemView.findViewById(R.id.cart_foodQuantity);
             btnEncrease = itemView.findViewById(R.id.btn_increase);
             btnDecrese = itemView.findViewById(R.id.btn_decrease);
+            btnDelete = itemView.findViewById(R.id.btn_delete_cart_item);
         }
     }
 }
